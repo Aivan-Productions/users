2d42477d667b571d0670d9619ea826054da73017
Alexey <45240041+Conte777@users.noreply.github.com>
2025-08-26 12:11:18 +0300

    Feat/add new files to parser/#20 (#25)
    * feat: add env parser
    
    Refs: #5
    
    * fix: Returning the default config without checks
    
    * feat: add .env.local and .env.production to parser
    
    * fix: insted of .env.example now .env
    
    closes: #20
    
    * fix: add comments
    
    * fix: delete unnecessary comments
    
    refs: #24
    
    * fix: delete unnecessary .env.example
    
    ---------
    
    Co-authored-by: Sergey <110046935+SushenaAr@users.noreply.github.com>

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/Aivan-Productions/users/tools/configuration"
 )
 
 func main() {
-	cfg := configuration.Load()
 
-	fmt.Print(cfg.PORT)
+	envFile := flag.String("env", ".env.local", "path to env file")
+	flag.Parse()
+
+	cfg := configuration.LoadConfig(*envFile)
+
+	fmt.Println(cfg.PORT)
+	fmt.Println(cfg.LOG_FORMAT)
 }
--- tools/configuration/configuration.go
+++ tools/configuration/configuration.go
@@ -3,40 +3,51 @@ package configuration
 import (
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/caarlos0/env/v11"
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	PORT int `env:"PORT" envDefault:"2000"`
+	PORT       int    `env:"PORT" envDefault:"8080"`
+	LOG_FORMAT string `env:"LOG_FORMAT" envDefault:"text"`
 }
 
 func defaultConfig() *Config {
 	return &Config{
-		PORT: 2000,
+		PORT:       8080,
+		LOG_FORMAT: "text",
 	}
 }
 
 func validate(cfg *Config) {
 	if cfg.PORT <= 0 || cfg.PORT > 65535 {
-		cfg.PORT = 2000
+		cfg.PORT = 8080
+	}
+	if !slices.Contains([]string{"text", "json"}, cfg.LOG_FORMAT) {
+		cfg.LOG_FORMAT = "text"
 	}
 }
 
-func Load() *Config {
-	if _, err := os.Stat(".env"); err != nil {
-		panic("configuration: файл .env не найден")
+func loadVarToEnv(configFileName string) {
+	if _, err := os.Stat(configFileName); err != nil {
+		panic(fmt.Sprintf("configuration: file %s not found: %v", configFileName, err))
 	}
 
-	if err := godotenv.Load(".env"); err != nil {
-		panic(fmt.Sprintf("configuration: не удалось загрузить .env: %v", err))
+	if err := godotenv.Load(configFileName); err != nil {
+		panic(fmt.Sprintf("configuration: failed to load %s: %v", configFileName, err))
 	}
+}
+
+func LoadConfig(configFileName string) *Config {
+	loadVarToEnv(".env")
+	loadVarToEnv(configFileName)
 
 	cfg := defaultConfig()
 
 	if err := env.Parse(cfg); err != nil {
-		cfg = defaultConfig()
+		return cfg
 	}
 
 	validate(cfg)
